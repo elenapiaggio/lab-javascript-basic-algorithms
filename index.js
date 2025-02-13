a026465ceb76136992f60bbf8432737283852e2b
Elena Piaggio <elenapiaggio@gmail.com>
2025-02-13 23:34:35 +0100

    ADD bonus 1 and bonus 2

--- index.js
+++ index.js
@@ -60,9 +60,26 @@ if (resultLocalCompare < 0) {
 
 
 // Bonus 1
-let longText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab expedita possimus totam sapiente delectus, facilis ad aut aspernatur earum cupiditate quisquam atque, ipsa beatae iure ut aliquam ex, itaque debitis. Libero explicabo voluptatum saepe aspernatur laborum dolorum, minus id provident ipsam quia, recusandae reiciendis aperiam sunt dolore animi vero. Tenetur esse voluptatem doloribus quidem incidunt necessitatibus facilis laborum nobis ducimus! Ab amet recusandae impedit animi, officiis soluta possimus doloremque nam quas, harum tempora laboriosam praesentium? Tempore, voluptate, adipisci, alias illum earum quisquam error optio soluta animi autem repudiandae ipsam id.";
+const longText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab expedita possimus totam sapiente delectus, facilis ad aut aspernatur earum cupiditate quisquam atque, ipsa beatae iure ut aliquam ex, itaque debitis. Libero explicabo voluptatum saepe aspernatur laborum dolorum, minus id provident ipsam quia, recusandae reiciendis aperiam sunt dolore animi vero. Tenetur esse voluptatem doloribus quidem incidunt necessitatibus facilis laborum nobis ducimus! Ab amet recusandae impedit animi, officiis soluta possimus doloremque nam quas, harum tempora laboriosam praesentium? Tempore, voluptate, adipisci, alias illum earum quisquam error optio soluta animi autem repudiandae ipsam id.";
 
 const words = longText.split(" ");
 const totalWords = words.length;
 
 console.log(`In total, there are: ${totalWords} words`);
+
+
+// Bonus 2
+// let phraseToCheck = "Amor, Roma";
+let phraseToCheck = "Was it a car or a cat I saw?";
+//let phraseToCheck = "taco cat";
+let cleanPhrase = "";
+for (let i = 0; i < phraseToCheck.length; i++) {
+  if (phraseToCheck[i] != " " && phraseToCheck[i] !== "," && phraseToCheck[i] != "." && phraseToCheck[i] != "?") {
+    cleanPhrase += phraseToCheck[i].toLowerCase();
+  }
+}
+if (cleanPhrase === cleanPhrase.split("").reverse().join("")) {
+  console.log(`${phraseToCheck} is a Palindrome`);
+} else {
+  console.log(`${phraseToCheck} is NOT a Palindrome`);
+}
